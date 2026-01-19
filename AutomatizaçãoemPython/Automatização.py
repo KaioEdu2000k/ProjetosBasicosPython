f82e00eb64b337820b48d680c01b710d4c3ddb4f
Kaio Eduardo <kaioeduardo.go@gmail.com>
2026-01-19 17:06:16 -0300

    Update Automatização.py

--- AutomatizaçãoemPython/Automatização.py
+++ AutomatizaçãoemPython/Automatização.py
@@ -1,1 +1,86 @@
+#100% funcional no meu PC. Requer alterações dependendo do PC.
+
+import pyautogui
+import time
+
+
+pyautogui.PAUSE = 1.5
+
+pyautogui.press("win")
+
+pyautogui.write("chrome")
+
+pyautogui.press("enter")
+
+pyautogui.click(x = 748, y= 575)
+
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+
+time.sleep(3)
+
+pyautogui.press("enter")
+
+
+pyautogui.click(x = 855, y = 372)
+
+pyautogui.write("soueu@gmail.com")
+
+pyautogui.press("tab")
+
+pyautogui.write("12345678910111215")
+
+pyautogui.press("tab")
+
+pyautogui.press("enter")
+
+time.sleep (5)
+
+import pandas as pd
+
+tabela = pd.read_csv("produtos.csv")
+
+print (tabela)
+
+for linha in tabela.index:
+
+    pyautogui.click(x = 765, y = 257)
+
+    codigo = str(tabela.loc[linha, "codigo"])
+    pyautogui.write(codigo)
+    pyautogui.press("tab")
+
+    marca = str(tabela.loc[linha, "marca"])
+    pyautogui.write(marca)
+    pyautogui.press("tab")
+
+    tipo = str(tabela.loc[linha, "tipo"])
+    pyautogui.write(tipo)
+    pyautogui.press("tab")
+
+    categoria = str(tabela.loc[linha, "categoria"])
+    pyautogui.write(categoria)
+    pyautogui.press("tab")
+
+    preco_unitario = str(tabela.loc[linha, "preco_unitario"])
+    pyautogui.write(preco_unitario)
+    pyautogui.press("tab")
+
+    custo = str(tabela.loc[linha, "custo"])
+    pyautogui.write(custo)
+    pyautogui.press("tab")
+
+    obs = str(tabela.loc[linha, "obs"])
+
+    if obs != "nan":
+        pyautogui.write(obs)
+    pyautogui.press("tab")
+
+    pyautogui.press("enter")
+
+    pyautogui.scroll(5000)
+
+
+   
+
+
 
